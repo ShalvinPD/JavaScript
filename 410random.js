3751663f397929328ba0332219781848faf3c839
Shalvin P D <shalvin@gmail.com>
2019-03-31 21:43:14 +0530

    Add files via upload

---
+++ 410random.js
@@ -0,0 +1,9 @@
+//Math.random() will return a value between 0 and 1
+console.log(Math.random())
+
+
+
+console.log(Math.round(Math.random() ))
+
+//Random number form 0 to 5
+console.log(Math.round(Math.random() * 5 ))
